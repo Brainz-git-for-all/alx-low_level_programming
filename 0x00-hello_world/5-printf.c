aa5e65562d009b886c01f7c6e0514672e2325028
Brainz-git-for-all <brainzgit@gmail.com>
2023-12-14 17:00:06 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-The main entry point
+ *Return: always zero
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
