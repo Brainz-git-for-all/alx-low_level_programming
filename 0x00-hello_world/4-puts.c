89fa9e83816773629bec4157729f238c0d111644
Brainz-git-for-all <brainzgit@gmail.com>
2023-12-14 16:20:45 +0000

    task 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *initialized
+ *main- The main function
+ *Return: always zero
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
