f391e7911017d8ded07aa6ae51a736880e058b3e
Brainz-git-for-all <brainzgit@gmail.com>
2023-12-15 20:21:35 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,13 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i == 9)
-		{
-			putchar(i + '0');
-		}
-		else
-		{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
-
 	}
+	putchar('\n');
 	return (0);
 }
