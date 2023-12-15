ab3104effd2cd008d00581f47d942dd09a43409d
Brainz-git-for-all <brainzgit@gmail.com>
2023-12-15 23:59:29 +0000

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *main-The main function
+ *Return:always (0)
+ */
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int l = n % 10;
+
+	if (l > 5)
+	{
+		printf("The last digit 0f %d is %d and is greater than 5\n", n, l);
+
+	}
+	else if (l == 0)
+	{
+		printf("The last digit 0f %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+	printf("%d is %d and is less than 6 and not equal to zero\n", n, l);
+	}
+	return (0);
+}
