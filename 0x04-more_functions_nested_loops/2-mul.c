1dad3c0e8bcad0536b7fcbcc58eec568eaad1cea
Brainz-git-for-all <brainzgit@gmail.com>
2023-12-21 18:17:05 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - checks whether the given string is upper case
+ *@a: the variable
+ *@b: another variable
+ *Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
