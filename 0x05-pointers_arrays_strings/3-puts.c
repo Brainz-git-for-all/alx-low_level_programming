6215e64805bf63c467400ecd70df7f31208ba623
Brainz-git-for-all <brainzgit@gmail.com>
2024-01-02 14:29:04 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts-prints the characters of a string
+ *@str: pointer
+ *Return:always zero
+ */
+
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	int i;
+
+	for (i = 0; i <= length; i++)
+	{
+		printf("%c", str[i])
+	}
+
+	printf("\n");
+}
