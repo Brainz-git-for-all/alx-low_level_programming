5c98b01159442a1709b52a1884796f25eb223561
Brainz-git-for-all <brainzgit@gmail.com>
2024-01-02 12:47:09 -0800

    task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98- this funtion points to 98
+ *@n: is the pointer
+ *Return: the pointer as 98
+ */
 
+void reset_to_98(int *n)
+{
+	int p = 98;
+	*n = &p;
+}
