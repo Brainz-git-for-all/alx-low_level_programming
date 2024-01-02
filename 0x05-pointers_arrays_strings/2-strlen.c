fc86f2f6aea0184e105de750d418ef23d6340ec1
Brainz-git-for-all <brainzgit@gmail.com>
2024-01-02 14:09:21 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- *_strlen:This function returns the length of a string
+ *_strlen- This function returns the length of a string
  *@s:is the pointer
  *Return: returns the length of a string
  */
 
 int _strlen(char *s)
 {
-	return (sizeof(*s));
+
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++
+	}
+
+	return (length);
 }
