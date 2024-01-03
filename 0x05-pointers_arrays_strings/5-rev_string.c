a75e25bf0777623b243be36720fc2c98c61e74fc
Brainz-git-for-all <brainzgit@gmail.com>
2024-01-02 17:11:22 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,14 @@ void rev_string(char *s)
 {
 	int length = 0;
 	int i;
-	
+
 	while (s[length] != '\0')
 		length++;
-	
-	for (i = length - 1; i >= 0; i--)
+
+	for (i = 0; i < length / 2; i++)
 	{
-		s[i];
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
 	}
 }
